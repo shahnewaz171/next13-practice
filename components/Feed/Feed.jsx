c15bd3d9d73495f76b939a28b4bb5500be919668
shahnewaz171 <shahnewaz601@gmail.com>
2023-09-24 00:18:40 +0600

    added professional level project structure

---
+++ components/Feed/Feed.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Feed = () => {
+  // state
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Feed;
